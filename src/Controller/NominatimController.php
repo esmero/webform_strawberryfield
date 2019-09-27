f8da3a15cbce3bf119fb2e41f981289622fc1a41
Diego Pino Navarro <dpino@metro.org>
2019-09-26 23:57:27 -0400

    ISSUE-16: How many Malibus are in the world? Will they still exist in 30 years?
    Adds Nominatim resolution to a quite complex working and silly-simplistic looking Webform Field. Has quite a lot of Ajax interaction and settings going here and there.  I extended an existing class and now i feel there is overhead on the parent which is not generic enough there. Will have to go back and implement some local methods to make this cleaner. But hey, it works!
    
    Tested on single form elements and Multiple ones (slightly different logic).
    Does not use all the n*3 elements we get via GEOJSON but a subset significat enough for metadata + the OSM id and type to get all again if needed.
    
    @TODO: various so will keep this open. I would love to have the time to add a visual hint via leaflet map for the values we extract. Probably that needs to go into format_strawberryfield.
    @TODO: better validation, what makes an entry like this valid?
    
      Also there are quite a few Malibus in the world, but for sanity we only return the first 5!
    
    Lastly. Since this is about geography, world, places, environment, lets put a  context statement here. Coding too long/MUCHO server+ screens and too much DevOps hurts the environment for real and Devs rarely care. Not kidding, we are wasting too much energy as Developers and users miss-using valious resources for just the sake of it/ just to keep the ball rolling, profit, consumism and even ego. Not fun. FridaysforFuture. starting in 4 minutes

---
+++ src/Controller/NominatimController.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Drupal\webform_strawberryfield\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Cache\CacheableJsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Url;
+
+/**
+ * Defines a route controller for Nominatim form elements.
+ *
+ * @see https://operations.osmfoundation.org/policies/nominatim/ for usage policies.
+ */
+class NominatimController extends ControllerBase implements ContainerInjectionInterface {
+
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\Client
+   */
+  protected $httpClient;
+
+
+  /**
+   * NominatimController constructor.
+   *
+   * @param \GuzzleHttp\Client $httpClient
+   */
+  public function __construct(Client $httpClient) {
+    $this->httpClient = $httpClient;
+  }
+
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *
+   * @return \Drupal\Core\Controller\ControllerBase|\Drupal\webform_strawberryfield\Controller\AuthAutocompleteController
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('http_client')
+      );
+  }
+
+  /**
+   * Handler for the Nominatim request.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @param $api_type
+   * @param $count
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
+  public function handleRequest(Request $request, $api_type = 'search', $count = 5, string $lang = '') {
+    $results = [];
+    //@TODO pass count to the actual fetchers
+    //@TODO maybe refactor into plugins so others can write any other reconciliators
+    //@TODO if so, we can query the plugins and show in the webform builder options
+    // Get the typed string from the URL, if it exists.
+    $lang = !empty($lang) ? trim($lang) : $this->languageManager()->getCurrentLanguage()->getId();
+    $results = [];
+    if ($input = $request->query->get('q')) {
+      switch($api_type) {
+        case 'search':  $results = $this->search($input,$count, $lang);
+        break;
+        case 'reverse': $results =  $this->reverse($input,$count, $lang);
+        break;
+      }
+
+
+    }
+    // Add Cache settings for Max-age and URL context.
+    // You can use any of Drupal's contexts, tags, and time.
+   /* $results['#cache'] = [
+      'max-age' => 600,
+      'contexts' => [
+        'url',
+      ],
+    ];*/
+    $response = new CacheableJsonResponse($results);
+    $response->addCacheableDependency($request->getRequestUri());
+    //$response->addCacheableDependency(CacheableMetadata::createFromRenderArray($results));
+    return $response;
+  }
+
+
+  /**
+   * @param $input
+   * @param int $count
+   * @param string $lang
+   *
+   * @return array
+   */
+  protected function search($input, int $count = 5, string $lang = 'en'){
+    // The request we are going to do is something like
+    // "https://nominatim.openstreetmap.org/search?q=West+87th+Street,NY&limit=5&format=geojson&addressdetails=1";
+    error_log($input);
+    error_log('running nominatim search for $input');
+    $remoteUrl = 'https://nominatim.openstreetmap.org/search';
+
+
+    $options['headers']=['Accept' => 'application/json', 'Accept-Language' => $lang];
+    $options['query'] = [
+      'q' => $input,
+      'limit' => $count,
+      'format' => 'geojson',
+      'addressdetails' => 1
+    ];
+    // Add an artificial delay of 1 second to aid in
+    // https://operations.osmfoundation.org/policies/nominatim/
+    sleep(1);
+    $body = $this->getRemoteJsonData($remoteUrl, $options);
+
+    $results = [];
+    $jsondata = json_decode($body, TRUE);
+    $json_error = json_last_error();
+    if ($json_error == JSON_ERROR_NONE) {
+      // nominatim will always return at least
+      // {"type":"FeatureCollection","licence":"Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright","features":[]}
+      if (isset($jsondata['type']) && $jsondata['type'] == 'FeatureCollection') {
+        if (isset($jsondata['features'])) {
+          foreach ($jsondata['features'] as $feature) {
+            if ($feature["type"] == "Feature") {
+              $results[] = [
+                'label' => isset($feature["properties"]["display_name"]) ? $feature["properties"]["display_name"] : $this->t('unknown name for place'),
+                'value' => $feature,
+              ];
+            }
+          }
+        }
+      }
+      else {
+        // Means our call was correct, but nominatim failed. Could be temporary?
+          $results[] = [
+            'value' => NULL,
+            'label' => 'Sorry https://nominatim.openstreetmap.org responsed with an error. Please Try again'
+          ];
+        }
+      return  $results;
+    }
+    $this->messenger->addError(
+      $this->t('Looks like data fetched from @url with query options: @options is not in JSON format.<br> JSON says: @jsonerror <br>Please check your URL!',
+        [
+          '@url' => $remoteUrl,
+          '@jsonerror' => $json_error,
+          '@options' => http_build_query($options['query']),
+        ]
+      )
+    );
+    return [];
+  }
+
+
+
+  /**
+   * @param $remoteUrl
+   * @param $options
+   *
+   * @return array|string
+   */
+  protected function getRemoteJsonData($remoteUrl, $options) {
+    // This is expensive, reason why we process and store in cache
+    if (empty($remoteUrl)){
+      // No need to alarm. all good. If not URL just return.
+      return [];
+    }
+    if (!UrlHelper::isValid($remoteUrl, $absolute = TRUE)) {
+      $this->messenger->addError(
+        $this->t('We can not fetch Data from @remoteUrl with query @options, check your URL',
+          [
+            '@remoteUrl' =>  $remoteUrl,
+            '@options' => http_build_query($options['query']),
+          ]
+        )
+      );
+      return [];
+    }
+
+
+    try {
+      $request = $this->httpClient->get($remoteUrl, $options);
+    }
+    catch(ClientException $exception) {
+      $responseMessage = $exception->getMessage();
+      $this->messenger->addError(
+        $this->t('We tried to contact @url with query @options but we could not. <br> The WEB says: @response. <br> Check that URL!',
+          [
+            '@url' => $remoteUrl,
+            '@response' => $responseMessage,
+            '@options' => http_build_query($options['query']),
+          ]
+        )
+      );
+      return [];
+    }
+    catch (ServerException $exception) {
+      $responseMessage = $exception->getMessage();
+      $this->loggerFactory->get('webform_strawberryfield')->error('We tried to contact @url with query @options but we could not. <br> The Remote server says: @response. <br> Check your query',
+          [
+            '@url' => $remoteUrl,
+            '@response' => $responseMessage,
+            '@options' => http_build_query($options['query']),
+          ]
+      );
+      return [];
+    }
+    $body = $request->getBody()->getContents();
+    return $body;
+  }
+
+}
