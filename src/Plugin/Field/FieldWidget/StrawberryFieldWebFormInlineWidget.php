c13006661032236c1c3c00f2ce00f0b0e27090f6
Diego Pino Navarro <dpino@metro.org>
2020-04-29 00:17:27 -0400

    Delete Temporary Webform Storage on Node Insert/Save
    This committ tries to fix many things at the same time but also brings new questions: like what is the expected behavior at all?
    
    What i do now here is once a Node/ADO is persistet, any temporary data used by the webform is wiped out. BUT! If someone, during a form ingest, just presses 'Save metadata' and leaves without saving a node, that Session is autoloaded the next time the user comes back. This is implemented here just for the Inline Webform and needs to be done for the AJAX driven one too. Also, i want to allow people to say 'hay, i don't want to load anything, just remove the old session'. That code is still missing. This should fix some issues but will bring now more questions, sadly i'm alone answering this =(

--- src/Plugin/Field/FieldWidget/StrawberryFieldWebFormInlineWidget.php
+++ src/Plugin/Field/FieldWidget/StrawberryFieldWebFormInlineWidget.php
@@ -136,6 +136,7 @@ class StrawberryFieldWebFormInlineWidget extends WidgetBase implements Container
     // Where is this field being used, a node?
     $entity_type = $items->getEntity()->getEntityTypeId();
     $bundle = $items->getEntity()->bundle();
+    $bundle_label = $items->getEntity()->type->entity->label();
     // So does the current loaded entity, where this widget is shown
     // has an id? If it has means we are editing!
     // We can always check via $items->getEntity()->isNew()
@@ -164,6 +165,7 @@ class StrawberryFieldWebFormInlineWidget extends WidgetBase implements Container
       $entity_uuid = $items->getEntity()->uuid();
       $entity_id = $items->getEntity()->id();
     }
+    $form_state->set('strawberryfield_webform_bundle_label', $bundle_label);
 
     // We will identify this widget amongst others using its form parents
     // as sha1 seed we use something unique what will stay the same across
@@ -254,6 +256,27 @@ class StrawberryFieldWebFormInlineWidget extends WidgetBase implements Container
     );
 
     $savedvalue = $items[$delta]->getValue();
+    $tempstore = \Drupal::service('tempstore.private')->get('archipel');
+    $tempstoreId = $this_widget_id;
+
+    /* @var $tempstore \Drupal\Core\TempStore\PrivateTempStore */
+    // Which means an abandoned Metadata Sessions somewhere
+    // Someone saved 'metadata' during a form update and left for coffee
+    // WE can reuse!
+    if (($tempstore->getMetadata($tempstoreId) != NULL) && $items->getEntity()->isNew()) {
+      $json_string = $tempstore->get($tempstoreId);
+      $json = json_decode($json_string, TRUE);
+      $json_error = json_last_error();
+      if ($json_error == JSON_ERROR_NONE) {
+        $savedvalue['value'] = $json_string;
+      }
+      $element['strawberry_webform_inline_message'] = [
+          '#type' => 'item',
+          '#title' => $this
+            ->t('Resuming metadata session:'),
+          '#markup' =>  $this->t('We found and loaded a previous unfinished metadata session for you.')
+      ];
+    }
 
     // Autofill from a previus submision if current value is empty
     $webform_autofill = empty($savedvalue['value']) || $items->getEntity()->isNew();
@@ -370,16 +393,29 @@ class StrawberryFieldWebFormInlineWidget extends WidgetBase implements Container
 
   public function validateWebform($element, FormStateInterface $form_state) {
     // Validate
-    // We have two states really:
-    // If old/loaded value is not empty then
+
     $tempstoreId = $form_state->get('strawberryfield_webform_widget_id');
     /* @var $tempstore \Drupal\Core\TempStore\PrivateTempStore */
 
     $tempstore = \Drupal::service('tempstore.private')->get('archipel');
-    if ($tempstore->getMetadata($tempstoreId) == NULL) {
-        // Means its empty. This can be Ok if something else than "save"
-        // Is triggering the Ajax Submit action like the "Display Switch"
-        // @TODO investigate some #limit_validation_error options to avoid that
+    if ($tempstore->getMetadata($tempstoreId) == NULL ) {
+      // Means its empty. This can be Ok if something else than "save"
+      // Is triggering the Ajax Submit action like the "Display Switch"
+      // Or we are not enforcing (required) really any values
+      if ($form_state->get('strawberryfield_webform_isnew')) {
+        // But if this a new Object and the tempstore is empty means
+        // No filling, no doing anything has happened
+        // And that is bad. So mark an error
+        $form_state->setError(
+          $element,
+          $this->t(
+            "Please complete all steps. You can not Save this @bundle_label without completing the required Form.",
+            [
+              '@bundle_label' => $form_state->get('strawberryfield_webform_bundle_label')
+            ]
+          )
+        );
+      }
         return;
     }
 
@@ -388,7 +424,7 @@ class StrawberryFieldWebFormInlineWidget extends WidgetBase implements Container
     $json_error = json_last_error();
     if ($json_error == JSON_ERROR_NONE) {
       $form_state->setValueForElement($element['strawberry_webform_widget']['json'], $json_string);
-      // Let tempstore entry expire, don't remove manually.
+      // Let tempstore entry expire or be removed by \Drupal\webform_strawberryfield\EventSubscriber\WebformStrawberryfieldDeleteTmpStorage
       return;
     }
     else {
