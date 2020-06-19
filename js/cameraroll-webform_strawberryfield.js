860c1ebe2783c52a9129572b4ff7b80a4bc11cb9
Diego Pino Navarro <dpino@metro.org>
2020-06-18 22:34:11 -0400

    Adds JP2 and MP4 to file upload elements
    See
    #47 and  #35
    @giancarlobi this should solve both problems in the simplest possible way.

--- js/cameraroll-webform_strawberryfield.js
+++ js/cameraroll-webform_strawberryfield.js
@@ -17,13 +17,19 @@
      */
     Drupal.behaviors.webformstrawberryCameraRoll = {
         attach: function (context, settings) {
-            console.log('hey!');
             // Only react if the document contains a strawberry webform widget
             if ($('.path-node fieldset[data-strawberryfield-selector="strawberry-webform-widget"]').length) {
                 $('input[type="file"].form-file').each(function(idx, item) {
                     console.log($(item).attr('accept'));
                     if ($(item).attr('accept') == 'image/*') {
-                        $(item).attr('accept', 'image/*;capture=camera');
+                        // We will add by default jp2. If the element is configured to not support it, that is Ok
+                        // Element validation handler will just complain and be will be safe.
+                        $(item).attr('accept', 'image/*;capture=camera;image/jp2');
+                    }
+                    else if ($(item).attr('accept') == 'video/*') {
+                        // We will add by default mp4. If the element is configured to not support it, that is Ok
+                        // Element validation handler will just complain and be will be safe.
+                        $(item).attr('accept', 'video/*;capture=camera;video/mp4');
                     }
                 });
             }
