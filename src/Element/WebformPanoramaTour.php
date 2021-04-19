5a3ac7137cc0d647ff88ef6cd42fe0c110486727
Diego Pino Navarro <dpino@metro.org>
2021-04-18 22:21:20 -0400

    Fixes missing URL value in. Hotspot info
    This requires https://github.com/esmero/format_strawberryfield/pull/152

--- src/Element/WebformPanoramaTour.php
+++ src/Element/WebformPanoramaTour.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\webform_strawberryfield\Element;
 
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element\FormElement;
 use Drupal\webform\Element\WebformCompositeBase;
@@ -838,8 +839,17 @@ class WebformPanoramaTour extends WebformCompositeBase {
       $newid = Uuid::uuid4();
       $hotspot->id = $element_name . '_' . $current_scene . '_' . $newid->toString();
       if ($hotspot->type == 'url') {
-        $hotspot->URL = $hotspot->url;
-        $hotspot->type = 'info';
+        $externalURL = $form_state->getValue(
+          [$element_name, 'hotspots_temp', 'url']
+        );
+        if (UrlHelper::isValid(trim($externalURL), TRUE)) {
+          $hotspot->URL = $externalURL;
+          $hotspot->type = 'info';
+        }
+        else {
+          $form_state->setRebuild(TRUE);
+          return;
+        }
       }
       if ($hotspot->type == 'ado') {
         $nodeid = $form_state->getValue(
@@ -881,7 +891,6 @@ class WebformPanoramaTour extends WebformCompositeBase {
         $hotspot->text = $hotspot->text;
         $hotspot->type = 'info';
       }
-
       $existing_objects[] = (array) $hotspot;
 
       // @TODO make sure people don't add twice the same coordinates!
@@ -891,7 +900,6 @@ class WebformPanoramaTour extends WebformCompositeBase {
       $allscenes[$scene_key]['hotspots'] = $existing_objects;
       $form_state->set($all_scenes_key, $allscenes);
       $form_state->setValue([$element_name, 'allscenes'], json_encode($allscenes));
-
 
     } else {
       // Do we alert the user? Form needs to be restarted
@@ -1014,7 +1022,7 @@ class WebformPanoramaTour extends WebformCompositeBase {
       }
 
       $form_state->set($all_scenes_key, $allscenes);
-      $form_state->setValue([$element_name, 'allscenes'],json_encode($allscenes));
+      $form_state->setValue([$element_name, 'allscenes'], json_encode($allscenes));
 
 
     } else {
