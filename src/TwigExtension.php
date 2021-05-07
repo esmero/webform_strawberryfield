cb5ebcb9def01c39d281eeeeb3508cebcede03de
Pat Dunlavey <pat@born-digital.com>
2021-05-07 17:49:04 -0400

    webform_options_twig --- expand sbf_webform_select_element_option_label twig extension to work with taxonomy select lists

--- src/TwigExtension.php
+++ src/TwigExtension.php
@@ -44,9 +44,20 @@ class TwigExtension extends \Twig_Extension {
       $webform = Webform::load($webform_id);
       if($webform) {
         $element = $webform->getElement($element_id);
-        if(!empty($element) && !empty($element['#options']) && !empty($element['#options'][$option])) {
-          return $element['#options'][$option];
+        if (!empty($element)) {
+          if (!empty($element['#options'][$option])) {
+            return $element['#options'][$option];
+          }
+          elseif (!empty($element['#vocabulary'])) {
+            $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
+              ->loadByProperties(['tid' => $option, 'vid' => $element['#vocabulary']]);
+            $term = reset($term);
+            if($term && $term->label()) {
+              return $term->label();
+            }
+          }
         }
+
       }
     }
     return NULL;
@@ -65,12 +76,12 @@ class TwigExtension extends \Twig_Extension {
     string $webform_option_list_id,
     ?string $option = NULL
   ): ?string {
-    if(is_string($option) && $option) {
+    if (is_string($option) && $option) {
       $webform_option_list = WebformOptions::load($webform_option_list_id);
       if (!empty($webform_option_list)) {
-        $options = $webform_option_list->getOptions();
-        if (!empty($options) && !empty($options[$option])) {
-          return $options[$option];
+        $labels = $webform_option_list->getOptions();
+        if (!empty($labels) && !empty($labels[$option])) {
+          return $labels[$option];
         }
       }
     }
