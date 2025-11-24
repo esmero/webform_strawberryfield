dd6748fe0731f0cd95dfabc5888a8eb0c188009c
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2025-11-24 14:42:03 -0500

    Deprecation in JS/Autocomplete: (1.6.0 and 2.0.0) firstCharacterDenyList is the new one (previous naming was deprecated, good!)
    This applies to 1.6.0 too

--- js/metadataauth-webform_strawberryfield.js
+++ js/metadataauth-webform_strawberryfield.js
@@ -150,7 +150,7 @@
 
         var term = Drupal.autocomplete.extractLastTerm(event.target.value);
 
-        if (term.length > 0 && options.firstCharacterBlacklist.indexOf(term[0]) !== -1) {
+        if (term.length > 0 && options.firstCharacterDenyList.indexOf(term[0]) !== -1) {
             return false;
         }
 
