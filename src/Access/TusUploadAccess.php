1d1f363424579ef39269db134ab058eb91b7661b
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2026-09-24 15:32:56 -0400

    Make the Tus Upload Access Controller stronger
    WE do other checks, we had this only for completion, but now its actually doing what it should do.

--- src/Access/TusUploadAccess.php
+++ src/Access/TusUploadAccess.php
@@ -8,6 +8,7 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\webform\WebformInterface;
 use Drupal\webform\WebformSubmissionInterface;
 use Drupal\webform_strawberryfield\Plugin\WebformHandler\strawberryFieldharvester;
+use Drupal\webform_strawberryfield\Plugin\WebformHandler\strawberryFieldhandPicker;
 
 /**
  * Defines the custom access control handler for the webform Tus Upload routes.
@@ -15,7 +16,7 @@ use Drupal\webform_strawberryfield\Plugin\WebformHandler\strawberryFieldharveste
 class TusUploadAccess {
 
   /**
-   * Check that webform submission has a strawberryfield webform handler and the user can create bundled Nodes
+   * Check if the key used  + webform match/are a TUS and the webform is ADO driven.
    * @param \Drupal\webform\WebformSubmissionInterface $webform_submission
    *   A webform submission.
    * @param \Drupal\Core\Session\AccountInterface $account
@@ -26,11 +27,21 @@ class TusUploadAccess {
    */
   public function checkAccess(WebformInterface $webform, string $key, AccountInterface $account) {
     $possible_upload_element = $webform->getElement($key);
-    $all_upload_elements = $webform->getElementsManagedFiles();
-    $access_result = AccessResult::allowed();
-    //allowedIf()
-    // $access_result->addCacheTags(['config:webform.settings']);
+    $is_tus = FALSE;
+    $access_result = AccessResult::forbidden()->addCacheableDependency($webform)->cachePerUser();
+    if ($account->isAnonymous()) {
+      return $access_result;
+    }
+    if ($possible_upload_element && $possible_upload_element['#type'] == 'webform_tus_file') {
+      $is_tus = TRUE;
+    }
+    $handlers = $webform->getHandlers();
+    foreach ($handlers as $id => $webform_handler) {
+      if (($webform_handler instanceof strawberryFieldharvester || $webform_handler instanceof strawberryFieldhandPicker) && $webform_handler->isEnabled()) {
+        return AccessResult::allowedIf($is_tus)->addCacheableDependency($webform)
+        ->cachePerUser();
+      }
+    }
     return $access_result;
   }
-
 }
